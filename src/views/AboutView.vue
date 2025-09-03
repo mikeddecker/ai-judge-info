5c2fc8b899c8104d8e8fc61127fa36369dd87c34
Mike De Decker <mikeddecker@hotmail.com>
2025-09-03 19:56:17 +0200

    clear content & add tailwind

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>Let's add some text to it</p>
+    <p class="font-bold">Let's add some text to it</p>
+    <p class="text-red-800 text-xl">test</p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
 }
 </style>

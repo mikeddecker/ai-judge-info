c543edd2c1ebb1d5e699812c89e7dfb20f7ba536
Mike De Decker <mikeddecker@hotmail.com>
2025-09-06 11:16:45 +0200

    about view - filling some information

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,31 @@
+<script setup>
+import { computed, onMounted, ref } from 'vue'
+
+const about = ref(null)
+const name = computed(() => about.value ? about.value['name'] : 'me')
+const info = computed(() => about.value ? about.value['info'] : '')
+const phone = computed(() => about.value ? about.value['phone'] : '')
+const email = computed(() => about.value ? about.value['email'] : '')
+
+onMounted(async () => {
+  about.value = await fetch('data.json').then((response) =>
+    response.json().then((value) => value['about']),
+  )
+})
+</script>
+
 <template>
-  <h1>This is an about page</h1>
-  <p class="font-bold">Let's add some text to it</p>
-  <p class="text-red-800 text-xl">test</p>
+  <div class="w-250 place-items-center">
+    <h1>About {{ name }}</h1>
+    <img :src="about ? about['picture'] : 'me'" class="w-40"></img>
+    <p>{{ info }}</p>
+    <p>{{ email }}</p>
+    <p>{{ phone }}</p>
+  </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
 }
 </style>
+
